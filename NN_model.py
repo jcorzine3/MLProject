f28e8e1e62b4b1b30011951c48f04b559c4022c8
Jonathan Choi <jjxchoi@gmail.com>
2021-07-07 22:50:10 -0400

    Added NNs

---
+++ NN_model.py
@@ -0,0 +1,22 @@
+# first neural network with keras tutorial
+from numpy import loadtxt
+from keras.models import Sequential
+from keras.layers import Dense
+# load the dataset
+dataset = loadtxt('simple_combined_data.csv', delimiter=',')
+# split into input (X) and output (y) variables
+X = dataset[:, 0:6]
+y = dataset[:, 6]
+# define the keras model
+model = Sequential()
+model.add(Dense(12, input_dim=6, activation='relu'))
+model.add(Dense(5, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+# compile the keras model
+model.compile(loss='binary_crossentropy',
+              optimizer='adam', metrics=['accuracy'])
+# fit the keras model on the dataset
+model.fit(X, y, epochs=150, batch_size=10)
+# evaluate the keras model
+_, accuracy = model.evaluate(X, y)
+print('Accuracy: %.2f' % (accuracy*100))
---
+++ NN_model_multiple_injuries.py
@@ -0,0 +1,39 @@
+# mlp for regression with mse loss function
+from sklearn.datasets import make_regression
+from sklearn.preprocessing import StandardScaler
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.optimizers import SGD
+from matplotlib import pyplot
+from numpy import loadtxt
+
+#load dataset
+dataset = loadtxt('combined_data_reinjury.csv', delimiter=',')
+X = dataset[:, 0:5]
+y = dataset[:, 5]
+# standardize dataset
+X = StandardScaler().fit_transform(X)
+y = StandardScaler().fit_transform(y.reshape(len(y),1))[:,0]
+# split into train and test
+n_train = round(X.shape[0]/2)
+#n_train = 600
+trainX, testX = X[:n_train, :], X[n_train:, :]
+trainy, testy = y[:n_train], y[n_train:]
+# define model
+model = Sequential()
+model.add(Dense(25, input_dim=5, activation='relu', kernel_initializer='he_uniform'))
+model.add(Dense(1, activation='linear'))
+opt = SGD(lr=0.01, momentum=0.9)
+model.compile(loss='mean_squared_error', optimizer=opt)
+# fit model
+history = model.fit(trainX, trainy, validation_data=(testX, testy), epochs=100, verbose=0)
+# evaluate the model
+train_mse = model.evaluate(trainX, trainy, verbose=0)
+test_mse = model.evaluate(testX, testy, verbose=0)
+print('Train: %.3f, Test: %.3f' % (train_mse, test_mse))
+# plot loss during training
+pyplot.title('Loss / Mean Squared Error')
+pyplot.plot(history.history['loss'], label='train')
+pyplot.plot(history.history['val_loss'], label='test')
+pyplot.legend()
+pyplot.show()
