04d66f83c7eae08845785af7a3e2454afc6ed48f
Jonathan Choi <jjxchoi@gmail.com>
2021-08-02 14:30:14 -0400

    implement PCA in NN_model and update multiple injuries model

--- NN_model.py
+++ NN_model.py
@@ -1,27 +1,30 @@
-# first neural network with keras tutorial
 from numpy import loadtxt
 from keras.models import Sequential
 from keras.layers import Dense
 from matplotlib import pyplot
+import PCA
 
-# load the dataset
 dataset = loadtxt('simple_combined_data.csv', delimiter=',')
-# split into input (X) and output (y) variables
-X = dataset[:, 0:6]
+# number of inputs to be reduced to after PCA
+inputs = 4
+
+X = PCA.pca(inputs)
 y = dataset[:, 6]
-# define the keras model
+# define model
 model = Sequential()
-model.add(Dense(12, input_dim=6, activation='relu'))
+model.add(Dense(12, input_dim=inputs, activation='relu'))
 model.add(Dense(5, activation='relu'))
 model.add(Dense(1, activation='sigmoid'))
-# compile the keras model
+# compile model
 model.compile(loss='binary_crossentropy',
               optimizer='adam', metrics=['accuracy'])
-# fit the keras model on the dataset
+# fit model on dataset
 history = model.fit(X, y, epochs=40, batch_size=10)
-# evaluate the keras model  
+# evaluate  model
 _, accuracy = model.evaluate(X, y)
 print('Accuracy: %.2f' % (accuracy*100))
 
-pyplot.title('Loss Over Epochs')
+# plot model
+pyplot.title('Loss Over Epochs with ' + str(inputs)+'-Feature PCA')
 pyplot.plot(history.history['loss'], label='train')
+pyplot.show()
--- PCA.py
+++ PCA.py
@@ -4,17 +4,24 @@ from pandas import read_csv
 from sklearn.decomposition import PCA
 # load data
 
-fieldnames = ['name','injury date','return date','days missed','injury type','age', 'height', 'weight',
-                'times previously injured', 'reinjured']
-dataframe = read_csv("combined_data.csv", names=fieldnames)
-array = dataframe.values
-X = array[1:,3:9] # 6 features: days missed, injury type, age, height, weight, times previously injured
-Y = array[1:,9]   # boolean output: reinjuried within a year
-# feature extraction
-pca = PCA(n_components=5)
-fit = pca.fit(X)
-transformed_fit =pca.fit_transform(X)
-# summarize components
-print("Explained Variance: %s" % fit.explained_variance_ratio_)
-print("Singular Values: %s" % fit.singular_values_)
-print(fit.components_)
+
+def pca(n_components):
+    fieldnames = ['name', 'injury date', 'return date', 'days missed', 'injury type', 'age', 'height', 'weight',
+                  'times previously injured', 'reinjured']
+    dataframe = read_csv("combined_data.csv", names=fieldnames)
+    array = dataframe.values
+    # 6 features: days missed, injury type, age, height, weight, times previously injured
+    X = array[1:, 3:9]
+    Y = array[1:, 9:]   # boolean output: reinjuried within a year
+
+    # feature extraction
+    pca = PCA(n_components)
+    fit = pca.fit(X)
+    transformed_fit = pca.fit_transform(X)
+    # summarize components
+    print(transformed_fit.shape)
+    print("Explained Variance: %s" % fit.explained_variance_ratio_)
+    print("Singular Values: %s" % fit.singular_values_)
+    print(fit.components_)
+
+    return transformed_fit
